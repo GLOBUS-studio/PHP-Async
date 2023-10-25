e81f6ed2994327e48cc0485fb0693275209e9442
GLOBUS.studio <admin@globus.studio>
2023-10-26 01:50:08 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,159 @@
-# PHPasync
+# Async PHP Library
+
+Bring modern asynchronous programming to PHP with the Async PHP Library, leveraging the capabilities of PHP fibers.
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+- [API Reference](#api-reference)
+  - [Async Class](#async-class)
+  - [AsyncManager Class](#asyncmanager-class)
+- [Contribution](#contribution)
+- [License](#license)
+
+## Features
+
+- **Async/Await Paradigm**: Intuitive async/await patterns.
+- **Task Prioritization**: Prioritize tasks with ease.
+- **Error Handling**: Powerful mechanisms with `then`, `catch`, and `finally`.
+- **Event System**: Incorporate custom events with `emit` and `on`.
+- **Timeouts**: Set timeouts to prevent overly lengthy operations.
+- **Cancellation**: Cancel tasks and operations dynamically.
+- **Progress Monitoring**: Stay updated during extended operations.
+- **Task Management**: Organize and execute tasks in batches.
+
+## Usage
+
+```php
+require 'path/to/library.php'; // Replace with the actual path to library
+
+// Sample tasks for demonstration
+function task1(Async $async): string {
+    sleep(1); // Simulating some delay
+    $async->progress(50); // Reporting task progress at 50%
+    sleep(1);
+    return "Task 1 completed";
+}
+
+function task2(Async $async): string {
+    sleep(3); // Simulating a longer delay
+    return "Task 2 completed";
+}
+
+function task3(Async $async): string {
+    sleep(2); // Simulating delay
+    $async->emit('customEvent', 'Data for custom event');
+    return "Task 3 completed with custom event";
+}
+
+function failingTask(Async $async): void {
+    throw new Exception("Error in task");
+}
+
+// Creating an asynchronous task
+$asyncTask1 = new Async('task1');
+
+// Using await method
+$result = $asyncTask1->await();
+echo $result . PHP_EOL; // Outputs "Task 1 completed"
+
+// Using the then method
+$asyncTask1->then(function($result) {
+    echo "Then: " . $result . PHP_EOL;
+});
+
+// Using the onProgress method to track progress
+$asyncTask1->onProgress(function($progress) {
+    echo "Task 1 progress: $progress%" . PHP_EOL;
+});
+
+// Using on method for custom events
+$asyncTask3 = new Async('task3');
+$asyncTask3->on('customEvent', function($data) {
+    echo "Received from custom event: $data" . PHP_EOL; // Outputs "Data for custom event"
+});
+
+// Using the catch method to handle errors
+$asyncTask2 = new Async('failingTask');
+$asyncTask2->catch(function($error) {
+    echo "An error occurred: " . $error->getMessage() . PHP_EOL; // Outputs "Error in task"
+});
+
+// Using the finally method
+$asyncTask2->finally(function() {
+    echo "Task 2 has finished (regardless of outcome)" . PHP_EOL;
+});
+
+// Using the timeout method
+$asyncTask4 = new Async('task2');
+$asyncTask4->timeout(1)->then(function($result) {
+    echo $result . PHP_EOL;
+})->catch(function($error) {
+    echo "An error occurred: " . $error->getMessage() . PHP_EOL; // Might output "Operation timed out."
+});
+
+// Using the start method
+$asyncTask5 = new Async(function($async) {
+    sleep(4);
+    return "Task 5 after manual start";
+});
+$asyncTask5->start();
+
+// Using the all and race methods
+$results = Async::all([$asyncTask1, $asyncTask3, $asyncTask4]);
+var_dump($results); // Outputs results of completed tasks
+
+$firstFinished = Async::race([$asyncTask1, $asyncTask3, $asyncTask4]);
+echo "First task to finish: $firstFinished" . PHP_EOL;
+
+// Using the setPriority method and task manager
+$asyncTask1->setPriority(1);
+$asyncTask3->setPriority(2);
+$asyncTask4->setPriority(3);
+AsyncManager::runTasks(); // Runs tasks based on their priority
+
+// Using the cancel method to cancel a task
+$asyncTask5->cancel();
+```
+
+## API Reference
+
+### Async Class
+
+Represents an individual asynchronous task, offering mechanisms to manage states, capture results or exceptions, and apply an event-driven approach.
+
+#### Methods:
+
+- `__construct(callable $callback)`: Initialize an asynchronous task.
+- `await()`: Wait and retrieve the operation's result.
+- `then(callable $onFulfilled, callable $onRejected = null)`: Handle operations that are either resolved or rejected.
+- `catch(callable $onRejected)`: Handle task errors.
+- `finally(callable $callback)`: Execute a callback post-task completion, irrespective of the outcome.
+- `on(string $event, callable $listener)`: Set event listeners.
+- `timeout(int $seconds)`: Define a timeout duration.
+- `cancel()`: Terminate the task.
+- `onProgress(callable $callback)`: Monitor progress.
+- `progress($data)`: Update progress data.
+- `start()`: Commence the async task.
+- `setPriority(int $priority)`: Assign priority.
+- `all(array $tasks)`: Awaits results from all specified async tasks.
+- `race(array $tasks)`: Awaits the result of the first completed task.
+
+### AsyncManager Class
+
+Manages multiple asynchronous tasks, offering prioritization among them.
+
+#### Methods:
+
+- `addTask(Async $task, int $priority = 0)`: Include a task and its optional priority.
+- `runTasks()`: Run all tasks in the queue.
+- `cancelAllOtherTasks(Async $completedTask)`: Cancel every task except the one specified.
+
+## Contribution
+
+Contributions are welcomed! Please open an issue or send in a pull request to enhance the library.
+
+## License
+
+This library falls under the MIT License. Refer to the `LICENSE.md` file for comprehensive details.
---
+++ phpasync.php
@@ -0,0 +1,293 @@
+<?php
+
+/**
+ * Manages asynchronous tasks with priorities.
+ *
+ * @package AsyncManager
+ * @author GLOBUS.studio <admin@globus.studio>
+ */
+
+use Fiber\Fiber;
+
+class AsyncManager
+{
+    private static $tasks = [];
+
+    public static function addTask(Async $task, int $priority = 0)
+    {
+        self::$tasks[] = [
+            'task' => $task,
+            'priority' => $priority
+        ];
+
+        usort(self::$tasks, function ($a, $b) {
+            return $b['priority'] - $a['priority'];
+        });
+    }
+
+    public static function runTasks()
+    {
+        while (count(self::$tasks) > 0) {
+            $task = array_shift(self::$tasks)['task'];
+            $task->start();
+        }
+    }
+
+    public static function cancelAllOtherTasks(Async $completedTask) {
+        foreach (self::$tasks as $taskInfo) {
+            $task = $taskInfo['task'];
+            if ($task !== $completedTask) {
+                $task->cancel();
+            }
+        }
+    }    
+}
+
+/**
+ * Represents an asynchronous task.
+ * 
+ * Allows to execute operations asynchronously using fibers, manage their states,
+ * and handle results or exceptions using event-driven approach.
+ *
+ * @package Async
+ * @author GLOBUS.studio <admin@globus.studio>
+ */
+
+class Async {
+
+    private $fiber;
+    private $result;
+    private $error;
+    private $listeners = [];
+    private $startTime;
+    private $hasTimeout = false;
+    private $isCancelled = false;
+
+    /**
+     * Initializes a new asynchronous task.
+     *
+     * @param callable $callback The operation to be executed asynchronously.
+     */    
+
+    public function __construct(callable $callback) {
+        $this->startTime = microtime(true);
+        $this->fiber = new Fiber(function () use ($callback) {
+            try {
+                while (!$this->isCancelled) {
+                    $this->result = $callback($this);
+
+                    if ($this->hasTimeout && (microtime(true) - $this->startTime) > $this->hasTimeout) {
+                        throw new Exception('Operation timed out.');
+                    }
+                    
+                    if (!$this->isCancelled) {
+                        $this->emit('resolve', $this->result);
+                    }
+                }
+            } catch (Throwable $e) {
+                $this->error = $e;
+                $this->emit('reject', $this->error);
+            } finally {
+                $this->emit('finally');
+            }
+        });
+    }
+
+    /**
+     * Awaits the completion of the asynchronous task.
+     *
+     * @return mixed The result of the asynchronous operation.
+     * @throws Exception If operation times out.
+     */
+
+    public function await(): mixed {
+        if ($this->hasTimeout && (microtime(true) - $this->startTime) > $this->hasTimeout) {
+            throw new Exception('Operation timed out.');
+        }
+
+        $this->fiber->start();
+        if ($this->error) {
+            throw $this->error;
+        }
+        return $this->result;
+    }
+
+    /**
+     * Registers a callback to be called when the asynchronous task is fulfilled.
+     *
+     * @param callable $onFulfilled Callback to be executed on task completion.
+     * @param callable|null $onRejected Optional callback to be executed on task failure.
+     * @return self Returns the current Async instance.
+     */
+
+    public function then(callable $onFulfilled, callable $onRejected = null) {
+        $this->on('resolve', $onFulfilled);
+        if ($onRejected) {
+            $this->on('reject', $onRejected);
+        }
+        return $this;
+    }
+
+    /**
+     * Registers a callback to be called when the asynchronous task is rejected.
+     *
+     * @param callable $onRejected Callback to be executed on task failure.
+     * @return self Returns the current Async instance.
+     */
+
+    public function catch(callable $onRejected) {
+        $this->on('reject', $onRejected);
+        return $this;
+    }
+
+    /**
+     * Registers a callback to be called regardless of the asynchronous task's outcome.
+     *
+     * @param callable $callback Callback to be executed after task completion or failure.
+     * @return self Returns the current Async instance.
+     */
+
+    public function finally(callable $callback) {
+        $this->on('finally', $callback);
+        return $this;
+    }
+
+    /**
+     * Registers an event listener for a specific event.
+     *
+     * @param string $event The event name.
+     * @param callable $listener The callback to be executed when the event is triggered.
+     * @return self Returns the current Async instance.
+     */
+
+    public function on(string $event, callable $listener) {
+        $this->listeners[$event][] = $listener;
+        return $this;
+    }
+
+    /**
+     * Emits/triggers a specified event with the provided data.
+     *
+     * @param string $event The event name to trigger.
+     * @param mixed $data Optional data to be passed to the event listeners.
+     * @return void
+     */
+
+    private function emit(string $event, $data = null) {
+        if (!isset($this->listeners[$event])) {
+            return;
+        }
+        foreach ($this->listeners[$event] as $listener) {
+            $listener($data);
+        }
+    }
+
+    /**
+     * Sets a timeout for the asynchronous operation.
+     *
+     * @param int $seconds The number of seconds before the operation times out.
+     * @return self Returns the current Async instance.
+     */
+
+    public function timeout(int $seconds) {
+        $this->hasTimeout = $seconds;
+        return $this;
+    }
+
+    /**
+     * Awaits and returns results of all provided asynchronous tasks.
+     *
+     * @param array $tasks An array of Async tasks.
+     * @return array The results of the asynchronous tasks.
+     */
+
+    public static function all(array $tasks): array {
+        $results = [];
+        foreach ($tasks as $key => $task) {
+            if ($task instanceof self) {
+                $results[$key] = $task->await();
+            }
+        }
+        return $results;
+    }
+
+    /**
+     * Awaits and returns the result of the first completed asynchronous task.
+     *
+     * @param array $tasks An array of Async tasks.
+     * @return mixed The result of the first completed task.
+     */
+
+    public static function race(array $tasks) {
+        foreach ($tasks as $task) {
+            if ($task instanceof self) {
+                $result = $task->await();
+                AsyncManager::cancelAllOtherTasks($task);
+                return $result;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Cancels the asynchronous task.
+     *
+     * @return void
+     */
+
+    public function cancel() {
+        $this->isCancelled = true;
+        $this->emit('cancel');
+    }
+
+    /**
+     * Registers a callback to be called to report progress of the asynchronous task.
+     *
+     * @param callable $callback Callback to be executed to report progress.
+     * @return self Returns the current Async instance.
+     */
+
+    public function onProgress(callable $callback) {
+        return $this->on('progress', $callback);
+    }
+
+    /**
+     * Reports progress of the asynchronous task.
+     *
+     * @param mixed $data Data about the progress.
+     * @return void
+     */
+
+    public function progress($data) {
+        $this->emit('progress', $data);
+    }
+
+    private $isStarted = false;
+
+    /**
+     * Starts the asynchronous task.
+     *
+     * @return void
+     */
+
+    public function start()
+    {
+        if (!$this->isStarted) {
+            $this->isStarted = true;
+            $this->fiber->start();
+        }
+    }
+
+    /**
+     * Sets the priority for the asynchronous task.
+     *
+     * @param int $priority The priority level.
+     * @return self Returns the current Async instance.
+     */
+
+    public function setPriority(int $priority)
+    {
+        AsyncManager::addTask($this, $priority);
+        return $this;
+    }
+}
